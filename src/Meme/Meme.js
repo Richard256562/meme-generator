11a512f1f3d003028cc7289c50686df859ede9cc
RichardOkongo <rirachard19@gmail.com>
2023-03-03 00:01:14 +0300

    added a generate button

--- src/Meme/Meme.js
+++ src/Meme/Meme.js
@@ -7,6 +7,13 @@ export const Meme = () => {
 
     const [memes, setMemes] = useState([]);
     const [memeIndex, setMemeIndex] = useState(0)
+    const [captions, setCaptions] = useState([]);
+
+
+    const updateCaption = (e, index) =>{
+      const text = e.target.value || '';
+      setCaptions
+    }
 
     const shuffleMemes = (array) => {
       for (let i = array.length - 1; i > 0; i--) {
@@ -21,18 +28,32 @@ export const Meme = () => {
         fetch('https://api.imgflip.com/get_memes').then(res => {
           res.json().then(res => {
             const _memes = res.data.memes;
+            shuffleMemes(_memes);
             setMemes(_memes);
+
         
             
           });
         });
       }, []); 
+      
+      useEffect(() => {
+        if(memes.length) {
+          setCaptions(Array(memes[memeIndex].box_count).fill(''));
+        }
+      }, [memeIndex, memes]);
     
     
     return (
         memes.length ?
         <div className={styles.container}>
+         <button onClick={generateMeme} className={styles.generate}>Generate</button>
           <button onClick={() => setMemeIndex(memeIndex + 1)} className={styles.skip}>Skip</button>
+          {
+        captions.map((c, index) => (
+          <input onChange={(e) => updateCaption(e, index)} key={index} />
+        ))
+      }
           <img src ={memes[memeIndex].url}/>
         </div>:
         <></>
