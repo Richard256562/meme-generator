e51b13e1258c8486f44538c54a524847f6ebde93
RichardOkongo <rirachard19@gmail.com>
2023-03-02 23:20:46 +0300

    made changes to the css

--- src/Meme/Meme.js
+++ src/Meme/Meme.js
@@ -3,11 +3,14 @@ import React, { useEffect, useState} from 'react';
 
 
 export const Meme = () => {
+
+    const [memes, setMemes] = useState([]);
     
     useEffect(() => {
         fetch('https://api.imgflip.com/get_memes').then(res => {
           res.json().then(res => {
             const _memes = res.data.memes;
+            setMemes(_memes);
         
             
           });
@@ -16,7 +19,12 @@ export const Meme = () => {
     
     
     return (
-    <>Hello</>
+        memes.length ?
+        <div>
+          <button>Skip</button>
+          <img src ={memes[0].url}/>
+        </div>:
+        <></>
     );
     
 }
